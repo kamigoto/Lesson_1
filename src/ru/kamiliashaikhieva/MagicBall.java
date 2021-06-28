6b23af57965ae1e989e920cea12842e1bf908d61
kamigoto <kamigotomoon@gmail.com>
2021-06-29 01:58:09 +0300

    added MagicBall class

---
+++ src/ru/kamiliashaikhieva/MagicBall.java
@@ -0,0 +1,27 @@
+package ru.kamiliashaikhieva;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+
+    public static void main(String[] args) {
+
+        System.out.println("Задвай свой вопрос");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String question = scanner.nextLine();
+
+
+        String anyAnswer[] = {"Возможно...", "Непременно!", "Скорее да", "Скорее нет", "Да", "Нет", "Сложный вопрос...", "Бесспорно", "Предрешено", "Никаких сомнений", "Определённо да", "Спроси позже", "Лучше не рассказывать",
+                "Сконцентрируйся и спроси опять", "Мой ответ - 'нет'", "Весьма сомнительно", "Сейчас нельзя предсказать"};
+
+
+        Random random = new Random();
+        int index = random.nextInt(anyAnswer.length);
+        System.out.println(anyAnswer[index]);
+
+    }
+}
+
