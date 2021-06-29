ad9e1609ea260fd29afac8d4cf09b8c9987621a2
kamigoto <kamigotomoon@gmail.com>
2021-06-29 12:02:01 +0300

    added DataTypes class

---
+++ src/ru/kamiliashaikhieva/DataTypes.java
@@ -0,0 +1,21 @@
+package ru.kamiliashaikhieva;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        System.out.println("Примитивные типы данных в Java");
+
+        System.out.println("Тип long: " + "минимум " + Long.MIN_VALUE + ", максимум " + Long.MAX_VALUE);
+        System.out.println("Тип int: " + "минимум " + Integer.MIN_VALUE + ", максимум " + Integer.MAX_VALUE);
+        System.out.println("Тип short: " + "минимум " + Short.MIN_VALUE + ", максимум " + Short.MAX_VALUE);
+        System.out.println("Тип byte: " + "минимум " + Byte.MIN_VALUE + ", максимум " + Byte.MAX_VALUE);
+        System.out.println("Тип char: " + "минимум " + (int) Character.MIN_VALUE + ", максимум " + (int) Character.MAX_VALUE);
+
+        System.out.println("Тип float: " + "минимум " + Float.MIN_VALUE + ", максимум " + Float.MAX_VALUE);
+        System.out.println("Тип double: " + "минимум " + Double.MIN_VALUE + ", максимум " + Double.MAX_VALUE);
+
+        System.out.println("Тип boolean: " + "возможные значения " + true + " или " + false);
+    }
+
+}
